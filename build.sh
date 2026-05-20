4d69e9a2718dd45ec94d147b707eddfec4ae5119
Leonardo Nápoles <leonapoles11@gmail.com>
2026-05-19 22:37:18 -0300

    update build script again

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
-export PATH="/rust/bin:$HOME/.cargo/bin:/root/.cargo/bin:$PATH"
-if [ -f "/rust/env" ]; then
-  source "/rust/env"
-elif [ -f "$HOME/.cargo/env" ]; then
-  source "$HOME/.cargo/env"
-fi
+
 rustup target add wasm32-unknown-unknown
+
 curl -L https://github.com/DioxusLabs/dioxus/releases/download/v0.7.0/dx-linux-x86_64 -o dx
 chmod +x dx
+
 ./dx build --release
